ca29f79ac6ec3b46dcea6aa9a8be86940449cd1e
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-11 11:02:29 +0530

    second commit

---
+++ selenium/src/main/java/selenium/Explicit.java
@@ -0,0 +1,33 @@
+package selenium;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v132.dom.DOM;
+import org.openqa.selenium.support.ui.ExpectedConditions; 
+import org.openqa.selenium.support.ui.WebDriverWait; 
+public class Explicit {
+public static void main(String[] args) throws InterruptedException {
+WebDriver driver=new ChromeDriver();
+WebDriverWait wait= new WebDriverWait(driver,Duration.ofSeconds(5)); 
+driver.manage().window().maximize();
+driver.get("https://www.selenium.dev/selenium/web/dynamic.html");
+
+//WebElement timerbutton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@id='Stats1_totalCount']")));
+//timerbutton.click();
+
+WebElement reveal = driver.findElement(By.xpath("//input[@id='reveal']"));
+reveal.click();
+
+WebElement revlead = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='revealed']")));
+revlead.sendKeys("Hello");
+//WebElement revlead1 = wait.until(ExpectedConditions.textToBePresentInElementLocated(revlead1));
+System.out.println(revlead.getDomAttribute("id"));
+
+System.out.println(revlead.getDomProperty("value"));
+
+}
+}
