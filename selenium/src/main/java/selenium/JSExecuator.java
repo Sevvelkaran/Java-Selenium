8660f1925534aa49bf22363241303e4e547d1ed2
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-12 14:51:53 +0530

    JSExecutor2

--- selenium/src/main/java/selenium/JSExecuator.java
+++ selenium/src/main/java/selenium/JSExecuator.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 
 public class JSExecuator {
 
@@ -16,11 +18,17 @@ public class JSExecuator {
 		driver.get("https://bstackdemo.com/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		WebElement option2=driver.findElement(By.xpath("//select"));
-		JavascriptExecutor js=(JavascriptExecutor) driver;
-		js.executeScript("arguments[0].value='lowestprice'", option2);
-	
+		WebElement ele = driver.findElement(By.xpath("//select"));
+//		JavascriptExecutor js=(JavascriptExecutor) driver;
+//		js.executeScript("arguments[0].value='lowestprice'", option2);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(ele).perform();
+		WebElement co = driver.findElement(By.xpath("//option[text()='Lowest to highest']"));
+		co.click();
 		
-				}
+		
+		driver.quit();
+		
+			}
 
 }
