3a5eff0d880358d4ed684892b496b2b916cf7fd5
Sevvelkaran <2k21it50@kiot.ac.in>
2025-03-12 12:03:05 +0530

    select1

---
+++ selenium/src/main/java/selenium/Select1.java
@@ -0,0 +1,41 @@
+package selenium;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Select1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://www.selenium.dev/selenium/web/formPage.html");
+		WebElement dd = driver.findElement(By.xpath("//select[@id = 'multi']"));
+		Select obj = new Select(dd);
+		List<WebElement> l1 = obj.getOptions();
+		for(WebElement ls : l1) {
+		System.out.println("The dropdown is : "+ls.getText());
+		}
+		boolean multiple = obj.isMultiple(); 
+		obj.selectByVisibleText("Eggs");
+		obj.selectByVisibleText("Onion gravy");
+		
+		List<WebElement> l2 = obj.getAllSelectedOptions();
+		for(WebElement lss : l2) {
+			System.out.println(lss.getText());
+		}
+		obj.deselectByIndex(0);
+		obj.deselectAll();
+
+	}
+	
+
+}
